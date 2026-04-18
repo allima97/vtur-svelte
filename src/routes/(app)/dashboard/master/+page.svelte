ee71d3a410082ed508dd01b9fc32f7b74d6a7379
André Lima <98129077+allima97@users.noreply.github.com>
2026-04-18 17:11:10 -0300

    feat(dashboard): add /dashboard/master route

---
+++ src/routes/(app)/dashboard/master/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+  import UnifiedDashboard from '$lib/dashboard/UnifiedDashboard.svelte';
+</script>
+
+<UnifiedDashboard title="Dashboard master" subtitle="Visão multi-empresa e consolidação global." />
