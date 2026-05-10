ede35d24833884e3da65f0aef4c1a180c8c9653e
test <test@test.com>
2026-05-10 12:38:29 -0300

    kpis

--- src/lib/components/kpis/KPIGrid.svelte
+++ src/lib/components/kpis/KPIGrid.svelte
@@ -8,10 +8,10 @@
 
   const gridClassMap: Record<string, string> = {
     auto: "vtur-kpi-grid",
-    "1": "vtur-kpi-grid !grid-cols-1",
-    "2": "vtur-kpi-grid !grid-cols-1 sm:!grid-cols-2",
-    "3": "vtur-kpi-grid !grid-cols-1 md:!grid-cols-3",
-    "4": "vtur-kpi-grid !grid-cols-1 sm:!grid-cols-2 lg:!grid-cols-4",
+    "1": "vtur-kpi-grid vtur-kpi-grid-1",
+    "2": "vtur-kpi-grid vtur-kpi-grid-2",
+    "3": "vtur-kpi-grid vtur-kpi-grid-3",
+    "4": "vtur-kpi-grid vtur-kpi-grid-4",
     "5": "vtur-kpi-grid vtur-kpi-grid-5",
     "6": "vtur-kpi-grid vtur-kpi-grid-6",
   };
