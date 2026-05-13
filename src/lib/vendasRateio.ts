6c7d7df54c8c415db38c8c47e5572eb35bad7680
André Lima <98129077+allima97@users.noreply.github.com>
2026-05-12 23:05:34 -0300

    otimização_659

--- src/lib/vendasRateio.ts
+++ src/lib/vendasRateio.ts
@@ -6,6 +6,8 @@ import {
 } from "$lib/utils/array";
 import { toCleanString as toStr, toFiniteNumber as toNumber } from "$lib/utils/values";
 
+const UUID_PATTERN = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+
 type RateioRow = {
   venda_recibo_id?: string | null;
   conciliacao_recibo_id?: string | null;
@@ -31,12 +33,7 @@ function normalizeCompanyScopeIds(companyId?: string | null, companyIds?: string
 }
 
 export function isUuid(value?: string | null) {
-  return Boolean(
-    value &&
-      /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(
-        String(value)
-      )
-  );
+  return Boolean(value && UUID_PATTERN.test(String(value)));
 }
 
 function scaleNumericField(value: unknown, factor: number) {
