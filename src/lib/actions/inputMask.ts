4ba76f4f15cae0112e2ecf23ba891f142c726775
André Lima <98129077+allima97@users.noreply.github.com>
2026-05-12 23:47:00 -0300

    otimização_672

--- src/lib/actions/inputMask.ts
+++ src/lib/actions/inputMask.ts
@@ -13,52 +13,66 @@ export interface MaskParams {
   type: MaskType;
 }
 
+const NON_DIGIT_RE = /\D/g;
+const CPF_FIRST_BLOCK_RE = /(\d{3})(\d)/;
+const CPF_LAST_BLOCK_RE = /(\d{3})(\d{1,2})$/;
+const CNPJ_FIRST_BLOCK_RE = /(\d{2})(\d)/;
+const CNPJ_MIDDLE_BLOCK_RE = /(\d{3})(\d)/;
+const CNPJ_SLASH_BLOCK_RE = /(\d{3})(\d)/;
+const CNPJ_LAST_BLOCK_RE = /(\d{4})(\d{1,2})$/;
+const PHONE_DDD_RE = /(\d{2})(\d)/;
+const PHONE_EIGHT_DIGIT_RE = /(\d{4})(\d{1,4})$/;
+const PHONE_NINE_DIGIT_RE = /(\d{5})(\d{1,4})$/;
+const CEP_BLOCK_RE = /(\d{5})(\d{1,3})$/;
+const DATE_FIRST_BLOCK_RE = /(\d{2})(\d)/;
+const DATE_LAST_BLOCK_RE = /(\d{2})(\d{1,4})$/;
+
 function applyMask(raw: string, type: MaskType): string {
-  const n = raw.replace(/\D/g, '');
+  const n = raw.replace(NON_DIGIT_RE, '');
 
   switch (type) {
     case 'cpf':
       return n
         .slice(0, 11)
-        .replace(/(\d{3})(\d)/, '$1.$2')
-        .replace(/(\d{3})(\d)/, '$1.$2')
-        .replace(/(\d{3})(\d{1,2})$/, '$1-$2');
+        .replace(CPF_FIRST_BLOCK_RE, '$1.$2')
+        .replace(CPF_FIRST_BLOCK_RE, '$1.$2')
+        .replace(CPF_LAST_BLOCK_RE, '$1-$2');
 
     case 'cnpj':
       return n
         .slice(0, 14)
-        .replace(/(\d{2})(\d)/, '$1.$2')
-        .replace(/(\d{3})(\d)/, '$1.$2')
-        .replace(/(\d{3})(\d)/, '$1/$2')
-        .replace(/(\d{4})(\d{1,2})$/, '$1-$2');
+        .replace(CNPJ_FIRST_BLOCK_RE, '$1.$2')
+        .replace(CNPJ_MIDDLE_BLOCK_RE, '$1.$2')
+        .replace(CNPJ_SLASH_BLOCK_RE, '$1/$2')
+        .replace(CNPJ_LAST_BLOCK_RE, '$1-$2');
 
     case 'phone':
       if (n.length <= 10) {
         return n
           .slice(0, 10)
-          .replace(/(\d{2})(\d)/, '($1) $2')
-          .replace(/(\d{4})(\d{1,4})$/, '$1-$2');
+          .replace(PHONE_DDD_RE, '($1) $2')
+          .replace(PHONE_EIGHT_DIGIT_RE, '$1-$2');
       }
       return n
         .slice(0, 11)
-        .replace(/(\d{2})(\d)/, '($1) $2')
-        .replace(/(\d{5})(\d{1,4})$/, '$1-$2');
+        .replace(PHONE_DDD_RE, '($1) $2')
+        .replace(PHONE_NINE_DIGIT_RE, '$1-$2');
 
     case 'cep':
-      return n.slice(0, 8).replace(/(\d{5})(\d{1,3})$/, '$1-$2');
+      return n.slice(0, 8).replace(CEP_BLOCK_RE, '$1-$2');
 
     case 'date':
       return n
         .slice(0, 8)
-        .replace(/(\d{2})(\d)/, '$1/$2')
-        .replace(/(\d{2})(\d{1,4})$/, '$1/$2');
+        .replace(DATE_FIRST_BLOCK_RE, '$1/$2')
+        .replace(DATE_LAST_BLOCK_RE, '$1/$2');
 
     case 'rg':
       return n
         .slice(0, 9)
-        .replace(/(\d{2})(\d)/, '$1.$2')
-        .replace(/(\d{3})(\d)/, '$1.$2')
-        .replace(/(\d{3})(\d{1,2})$/, '$1-$2');
+        .replace(CNPJ_FIRST_BLOCK_RE, '$1.$2')
+        .replace(CNPJ_MIDDLE_BLOCK_RE, '$1.$2')
+        .replace(CPF_LAST_BLOCK_RE, '$1-$2');
 
     default:
       return raw;
